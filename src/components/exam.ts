a534a82fd88a120f76ca97a67f97d445f9eea226
cppHusky <cpphusky@outlook.com>
2026-04-12 07:58:24 +0800

    replace <True> with <Option correct>

--- src/components/exam.ts
+++ src/components/exam.ts
@@ -1,10 +1,8 @@
 import Blank from "./exam/Blank.astro";
 import Choices from "./exam/Choices.astro";
-import False from "./exam/False.astro";
 import Figure from "./exam/Figure.astro";
 import Option from "./exam/Option.astro";
 import Slot from "./exam/Slot.astro";
 import Solution from "./exam/Solution.astro";
-import True from "./exam/True.astro";
 
-export {Blank,Choices,False,Figure,Option,Slot,Solution,True};
+export {Blank,Choices,Figure,Option,Slot,Solution};
